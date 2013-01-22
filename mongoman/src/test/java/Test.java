b7d3e13e415a2bf605a5c44b1cf761a164d3e2fd
jhshin9 <jhshin9@gmail.com>
2013-01-22 09:42:37 +0900

    First Commit

---
+++ mongoman/src/test/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Description.
+ * </p>
+ *
+ * @author <a href="mailto:jhshin9@gmail.com">신정훈</a>
+ * @version 1.0
+ * @since 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Mongo......");
+    }
+}
